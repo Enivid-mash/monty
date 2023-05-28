ad334710cdde3aea74b3d25cdd1a58b406f0fcaf
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-05-28 06:43:44 -0700

    Amend monty.c

--- monty.c
+++ monty.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(line, sizeof(line), file) != NULL)
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
 		}
 		if (!found)
 		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			fprintf(stderr, "L%d: Unknown instruction %s\n", line_number, opcode);
 			exit(EXIT_FAILURE);
 		}
 		line_number++;
