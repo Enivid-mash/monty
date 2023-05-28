25cc4ffe4007200819e7a7058797e853e4ded2e7
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-05-28 03:57:40 -0700

    Amend monty.c

--- monty.c
+++ monty.c
@@ -29,7 +29,6 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: monty filename\n");
-		fprintf(stderr, "\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
