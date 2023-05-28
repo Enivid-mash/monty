1bb2efba57d5559645ad71b4e41a6bbf0d906f95
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-05-28 03:01:01 -0700

    Add and amend headers

--- monty.c
+++ monty.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 
 instruction_t instructions[] = {
 	{"push", PUSH},
--- monty.h
+++ monty.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef MONTY_H_
+#define MONTY_H_ 
 
 #include <stdio.h>
 #include <stdlib.h>
